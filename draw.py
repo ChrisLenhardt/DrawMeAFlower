8809b146dbcfe4ba72f80a2f0d9bc911071cfe48
notSpainChris <116445138+notSpainChris@users.noreply.github.com>
2023-02-21 12:31:27 -0500

    Create draw.py

---
+++ draw.py
@@ -0,0 +1,20 @@
+import turtle
+turtle.home()
+
+#Make functions here bcuz python is dumb
+def makeBackground():
+    turtle.color("green")
+    turtle.dot(500)
+
+def makeSeedPod():
+    turtle.color("yellow")
+    turtle.dot(100)
+#begin cooking
+makeBackground()
+
+
+
+#wrap up drawing
+turtle.hideturtle()
+turtle.exitonclick()
+
